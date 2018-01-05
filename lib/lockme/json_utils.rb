17402e6760c266b81e13770ad7384aa55f85da55
Mike Szyndel <msz@sent.com>
2018-01-05 18:46:18 +0100

    refactoring

--- lib/lockme/json_utils.rb
+++ lib/lockme/json_utils.rb
@@ -6,8 +6,12 @@ module Lockme
   module JsonUtils
     def to_json
       utf_str = @data.to_h.to_json.force_encoding('UTF-8')
-      utf_str.each_codepoint.reduce(String.new) do |str, c|
-        str << (c > 127 ? '\u' + c.to_s(16).rjust(4, '0') : c.chr)
+      utf_str.each_codepoint.reduce(String.new) do |str, codepoint|
+        str << if codepoint > 127
+          '\u' + codepoint.to_s(16).rjust(4, '0')
+        else
+          codepoint.chr
+        end
       end
     end
   end
